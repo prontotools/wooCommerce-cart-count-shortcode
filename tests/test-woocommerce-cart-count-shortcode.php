7c125c08d3804020670b12367f044898184a2065
prontosandy <sandy@prontomarketing.com>
2016-03-31 14:43:59 +0700

    Make test pass :ok_woman:

--- tests/test-woocommerce-cart-count-shortcode.php
+++ tests/test-woocommerce-cart-count-shortcode.php
@@ -3,7 +3,7 @@
 require_once( "woocommerce-cart-count-shortcode.php" );
 
 class WooCommerce_Cart_Count_Shortcode_Test extends WP_UnitTestCase {
-    public function test_failed() {
-        $this->assertTrue( false );
+    public function test_should_pass() {
+        $this->assertTrue( true );
     }
 }
