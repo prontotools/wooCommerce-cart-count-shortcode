930e04c4be9ec35f1f26575101c97779789f0893
prontosandy <sandy@prontomarketing.com>
2016-03-31 13:36:46 +0700

    Add initial test :ok_woman:

---
+++ tests/test-woocommerce-cart-count-shortcode.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once( "woocommerce-cart-count-shortcode.php" );
+
+class WooCommerce_Cart_Count_Shortcode_Test extends WP_UnitTestCase {
+    public function test_failed() {
+        $this->assertTrue( false );
+    }
+}
