d4385fa7435654077e765723e8ac1d820c383c29
prontosandy <sandy@prontomarketing.com>
2016-03-31 15:47:14 +0700

    Add setUp and tearDown in test :ok_woman:

--- tests/test-woocommerce-cart-count-shortcode.php
+++ tests/test-woocommerce-cart-count-shortcode.php
@@ -3,6 +3,14 @@
 require_once( "woocommerce-cart-count-shortcode.php" );
 
 class WooCommerce_Cart_Count_Shortcode_Test extends WP_UnitTestCase {
+	public function setUp() {
+		parent::setUp();
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+	}
+
     public function test_should_pass() {
         $this->assertTrue( true );
     }
